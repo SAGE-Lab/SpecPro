84465676d1d888294ec97aa18b74d5ed6b890409
Simone Vuotto <simov8@gmail.com>
2018-03-23 11:21:30 +0100

    Update antlr4 version

--- src/main/java/snl2fl/fl/parser/FLGrammarBaseListener.java
+++ src/main/java/snl2fl/fl/parser/FLGrammarBaseListener.java
@@ -1,4 +1,4 @@
-// Generated from FLGrammar.g4 by ANTLR 4.5.3
+// Generated from FLGrammar.g4 by ANTLR 4.7.1
 package snl2fl.fl.parser;
 
 import org.antlr.v4.runtime.ParserRuleContext;
--- src/main/java/snl2fl/req/parser/RequirementsGrammarListener.java
+++ src/main/java/snl2fl/req/parser/RequirementsGrammarListener.java
@@ -1,4 +1,4 @@
-// Generated from RequirementsGrammar.g4 by ANTLR 4.5.3
+// Generated from RequirementsGrammar.g4 by ANTLR 4.7.1
 package snl2fl.req.parser;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 
